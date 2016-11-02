6e9f58ab9fcb085ea2da89ade7f72a9880b22c72
SuzukiTakumaEW <finalfantasy0712kuria@gmail.com>
2016-11-02 13:44:36 +0900

    あげ忘れ

---
+++ engine/TenshiEngine/ScriptComponent/Scripts/AimController.h
@@ -0,0 +1,33 @@
+
+#pragma once
+#include "main.h"
+#include "TPSCamera.h"
+
+class AimController :public IDllScriptComponent{
+public:
+	void Initialize()override;
+	void Start()override;
+	void Update()override;
+	void Finish()override;
+	void OnCollideBegin(GameObject target)override;
+	void OnCollideEnter(GameObject target)override;
+	void OnCollideExit(GameObject target)override;
+
+	void ChangeAimMode(TPSCamera* camera,GameObject player, bool isFree);
+
+	void PlayerRotate(TPSCamera* camera, GameObject player);
+
+private:
+	//ÉÅÉìÉoïœêî
+	SERIALIZE XMVECTOR mAimPoint;
+	SERIALIZE XMVECTOR mDefoultPoint;
+	SERIALIZE float mDefoultDamping;
+	SERIALIZE float mDefoultStiffness;
+	SERIALIZE float mDamping;
+	SERIALIZE float mStiffness;
+
+	//ìäÇ∞ÇÈóp
+	GameObject mWeaponHand;
+	//UI
+	GameObject mAimUISet;
+};
