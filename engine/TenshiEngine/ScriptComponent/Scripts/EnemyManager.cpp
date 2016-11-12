80c3625923ae640b8ad3db59ddecc0ae17a2b328
shibuyadaiki <shibuyadaiki684@gmail.com>
2016-11-13 03:55:19 +0900

    EnemyManagerで敵を管理する部分を作った。EnemyがEnemyManagerに対応していないので今は生成しか行っていない。(ノートPCへ転送用にプッシュ)

--- engine/TenshiEngine/ScriptComponent/Scripts/EnemyManager.cpp
+++ engine/TenshiEngine/ScriptComponent/Scripts/EnemyManager.cpp
@@ -16,39 +16,50 @@ void EnemyManager::Start(){
 
 //毎フレーム呼ばれます
 void EnemyManager::Update(){
+	if (Input::Trigger(KeyCode::Key_0))EnemyTeamIntoEnemyContainer(m_EnemyTeam);
+
 	for (auto i = m_EnemyContainer.begin(); i != m_EnemyContainer.end();) {
-		//親が生きているかどうか
-		bool parentAlive = false;
-		for (auto j = i->begin(); j != i->end();) {
-			if (j->Get()->EndFinish()) {
-				j = i->erase(j);
-			}
-			else {
-				///処理を書く
-				auto jScript = j->Get()->GetScript<Enemy>();
-				if (!jScript)return;
-				if (jScript->GetChildFlag()) {
-					if (parentAlive) {
-
-					}
-					else {
-
-					}
+		bool deleteTeam = false;
+		for (auto j = i->enemyOneVec.begin(); j != i->enemyOneVec.end();) {
+			if (j->enemyGameObject.Get()->EndFinish()) {
+				j = i->enemyOneVec.erase(j);
+				i->enemyTeamParameter.teamCount -= 1;
+				if (m_DrawFlag)Hx::Debug()->Log("Team" + std::to_string(i->enemyTeamParameter.teamID) + " : " + std::to_string(i->enemyTeamParameter.teamCount) + "人");
+				if (i->enemyOneVec.size() == 0) {
+					if (m_DrawFlag)Hx::Debug()->Log("Team" + std::to_string(i->enemyTeamParameter.teamID) + " : " + "チームが壊滅した");
+					i = m_EnemyContainer.erase(i);
+					deleteTeam = true;
+					break;
 				}
 				else {
-					//確実に親が先頭に入っているのでここで生存確認をする
-					parentAlive = true;
-
+					continue;
 				}
-				j++;
+			}
+
+			//発見したかどうか
+
+			j++;
+		}
+		if (deleteTeam)continue;
+		for (auto j = i->enemyOneVec.begin(); j != i->enemyOneVec.end();j++) {
+			auto jScript = j->enemyGameObject.Get()->GetScript<Enemy>();
+			if (!jScript)return;
+			if (j->enemyParameter.child) {
+				//子分の処理
+				if (i->enemyTeamParameter.parentAlive) {
+					//親が生きていたら
+				}
+				else {
+					//親が死んでいたら
+				}
+				//どちらでも
+
+			}
+			else {
+				//親分の処理
 			}
 		}
-		if (i->size() == 0) {
-			i = m_EnemyContainer.erase(i);
-		}
-		else {
-			i++;
-		}
+		i++;
 	}
 }
 
@@ -76,18 +87,51 @@ void EnemyManager::EnemyTeamIntoEnemyContainer(GameObject g){
 	auto newEnemyTeam = Hx::Instance(g);
 	if (!newEnemyTeam)return;
 	newEnemyTeam->Enable();
-	auto enemyTeamChild = newEnemyTeam->mTransform->Children();
-	if (enemyTeamChild.size() == 0)return;
-	std::vector<GameObject> newEnemyTeamChildVec;
-	for (auto i : newEnemyTeam->mTransform->Children()) {
-		if (!i)return;
-		auto iScript = i->GetScript<Enemy>();
-		if (iScript->GetChildFlag()) {
-			newEnemyTeamChildVec.push_back(i);
-		}
-		else{
-			newEnemyTeamChildVec.insert(newEnemyTeamChildVec.begin(), i);
+	EnemyOne eo;
+	EnemyTeam et;
+	int count = 0;
+	if (newEnemyTeam->GetScript<Enemy>()) {
+		count++;
+		eo.enemyGameObject = newEnemyTeam;
+		eo.enemyParameter.child = false;
+		eo.enemyParameter.nowActionMode = ACTIONMODE::TRACKINGMODE;
+		eo.enemyParameter.nowTrackingAction = TRACKINGACTION::PARENTTRACKING;
+		et.enemyOneVec.insert(et.enemyOneVec.begin(), eo);
+	}
+	else {
+		auto enemyTeamChild = newEnemyTeam->mTransform->Children();
+		if (enemyTeamChild.size() == 0)return;
+
+		for (auto i : enemyTeamChild) {
+			count++;
+			if (!i)return;
+			auto iScript = i->GetScript<Enemy>();
+			eo.enemyGameObject = i;
+			if (iScript->GetChildFlag()) {
+				eo.enemyParameter.child = true;
+				eo.enemyParameter.nowActionMode = ACTIONMODE::TRACKINGMODE;
+				eo.enemyParameter.nowTrackingAction = TRACKINGACTION::CHILDTRACKING;
+				et.enemyOneVec.push_back(eo);
+			}
+			else {
+				eo.enemyParameter.child = false;
+				eo.enemyParameter.nowActionMode = ACTIONMODE::TRACKINGMODE;
+				eo.enemyParameter.nowTrackingAction = TRACKINGACTION::PARENTTRACKING;
+				et.enemyOneVec.insert(et.enemyOneVec.begin(), eo);
+			}
 		}
 	}
-	m_EnemyContainer.push_back(newEnemyTeamChildVec);
+
+	et.enemyTeamParameter.teamCount = count;
+
+	count = 0;
+	for (auto i = m_EnemyContainer.begin(); i != m_EnemyContainer.end(); i++) {
+		if (i->enemyTeamParameter.teamID == count) {
+			count++;
+			i = m_EnemyContainer.begin();
+		}
+	}
+	et.enemyTeamParameter.teamID = count;
+	if (m_DrawFlag)Hx::Debug()->Log("Team" + std::to_string(et.enemyTeamParameter.teamID) + " : " + std::to_string(et.enemyTeamParameter.teamCount) + "人");
+	m_EnemyContainer.push_back(et);
 }
--- engine/TenshiEngine/ScriptComponent/Scripts/EnemyManager.h
+++ engine/TenshiEngine/ScriptComponent/Scripts/EnemyManager.h
@@ -1,7 +1,60 @@
 
 #pragma once
 #include "main.h"
+#include "Enemy.h"
 
+struct EnemyParameter {
+	//子分かどうか
+	bool child = true;
+
+	//今のアクションモード(TRACKING or BATTLE)のID
+	ACTIONMODE nowActionMode = ACTIONMODE::TRACKINGMODE;
+
+	//今の捜索中のアクションのID
+	TRACKINGACTION::Enum nowTrackingAction = TRACKINGACTION::NONE;
+
+	//前の捜索中のアクションのID
+	TRACKINGACTION::Enum beforeTrackingAction = TRACKINGACTION::NONE;
+
+	//今の戦闘中のアクションのID
+	BATTLEACTION::Enum nowBattleAction = BATTLEACTION::NONE;
+
+	//前の戦闘中のアクションのID
+	BATTLEACTION::Enum beforeBattleAction = BATTLEACTION::NONE;
+};
+
+struct EnemyOne {
+	//個々の敵のgameObject
+	GameObject enemyGameObject;
+
+	//個々の敵のパラメータ
+	EnemyParameter enemyParameter;
+};
+
+struct EnemyTeamParameter {
+	//チームの番号
+	int teamID = 0;
+
+	//チームの人数
+	int teamCount = 0;
+
+	//誰が攻撃するか
+	int whoAttack = 0;
+
+	//見つけているか
+	bool discovery = false;
+
+	//親が生きているかどうか
+	bool parentAlive = true;
+};
+
+struct EnemyTeam {
+	//個々の敵のgameObjectとパラメータをまとめたものがチーム分入ってるもの
+	std::vector<EnemyOne> enemyOneVec;
+
+	//チームのパラメータ
+	EnemyTeamParameter enemyTeamParameter;
+};
 
 class EnemyManager :public IDllScriptComponent{
 public:
@@ -17,11 +70,13 @@ private:
 	//メンバ変数
 	SERIALIZE
 	GameObject m_EnemyTeam;
+	SERIALIZE
+	bool m_DrawFlag;
 
 	/// <summary>
 	///Enemyの入れ物へGameObjectを入れる
 	/// </summary>
 	void EnemyTeamIntoEnemyContainer(GameObject g);
 
-	std::vector<std::vector<GameObject>> m_EnemyContainer;
+	std::vector<EnemyTeam> m_EnemyContainer;
 };
