eb32695ed3600df564fa049d4850156b14ed521f
HxTneshi <hinanayi.tenshi@gmail.com>
2017-01-19 13:55:16 +0900

    UniqueObjectの取得処理

---
+++ engine/TenshiEngine/ScriptComponent/Scripts/UniqueObject.h
@@ -0,0 +1,19 @@
+
+#pragma once
+#include "main.h"
+
+
+class UniqueObject :public IDllScriptComponent{
+public:
+	UniqueObject();
+
+	void Finish()override;
+
+	static GameObject GetPlayer();
+	static GameObject GetCamera();
+
+private:
+	//ƒƒ“ƒo•Ï”
+	SERIALIZE GameObject m_Player;
+	SERIALIZE GameObject m_Camera;
+};
