2bb8349f4eb4932d08ed709479ed8f72e5b3525d
HxTneshi <hinanayi.tenshi@gmail.com>
2017-01-25 16:43:22 +0900

    レイス開始

---
+++ engine/TenshiEngine/ScriptComponent/Scripts/reis.h
@@ -0,0 +1,38 @@
+
+#pragma once
+#include "main.h"
+
+
+class reis :public IDllScriptComponent{
+public:
+	void Initialize()override;
+	void Start()override;
+	void Update()override;
+	void Finish()override;
+	void OnCollideBegin(GameObject target)override;
+	void OnCollideEnter(GameObject target)override;
+	void OnCollideExit(GameObject target)override;
+
+private:
+	void Move(const XMVECTOR& vect);
+	void Rotate(const XMVECTOR& vect);
+	XMVECTOR GetPlayerVect();
+	float GetPlayerLen();
+	float GetPlayerLenH();
+	float GetPlayerLenV();
+
+	void ChangeAnime(int id);
+
+	bool IsCurrentAnimeEnd();
+
+	//ÉÅÉìÉoïœêî
+
+
+	SERIALIZE GameObject m_AnimeModel;
+	SERIALIZE float m_MoveSpeed;
+
+
+	GameObject m_Player;
+	int m_ReisMode;
+	int m_CurrentAnimeID;
+};
