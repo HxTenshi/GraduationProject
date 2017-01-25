43812c95fcf429373bf5debbcec9196179c9cfb9
funifuni <kashisu.enoki@gmail.com>
2017-01-25 12:25:02 +0900

    修正

--- engine/TenshiEngine/ScriptComponent/Scripts/Weapon.cpp
+++ engine/TenshiEngine/ScriptComponent/Scripts/Weapon.cpp
@@ -47,14 +47,7 @@ void Weapon::Update(){
 	if (is_attack == 2) {
 		is_attack = 3;
 	}
-	if (Input::Trigger(KeyCode::Key_C)) {
-		if (m_WeaponEffect) {
-			auto scr = m_WeaponEffect->GetScript<WeaponEffect>();
-			scr->Action();
-		}
-	
 
-	}
 	BreakWeapon();
 	m_Recast += 1 * Hx::DeltaTime()->GetDeltaTime();
 	ThrowAwayAction();
@@ -343,8 +336,13 @@ bool Weapon::isAttack()
 
 void Weapon::SetAttackFlag(bool flag)
 {
+
 	if (flag) {
 		if (is_attack == 0) {
+			if (m_WeaponEffect) {
+				auto scr = m_WeaponEffect->GetScript<WeaponEffect>();
+				scr->Action();
+			}
 			is_attack = 1;
 		}
 	}
